3310b0c20ea7b4d13ac8f0806748bb25cf549bb2
Homin Lee <homin.lee@suapapa.net>
2020-05-23 09:44:21 +0900

    _ref: move gopher eye

--- _ref/main.go
+++ _ref/main.go
@@ -9,13 +9,76 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
-	"strconv"
+	"path/filepath"
 	"strings"
 	"syscall/js"
 	"time"
 )
+
+var (
+	images map[string]js.Value
+	r      *rand.Rand
+)
+
+func init() {
+	// random generator
+	src := rand.NewSource(time.Now().UnixNano())
+	r = rand.New(src)
+}
+
+func main() {
+	doc := js.Global().Get("document")
+	canvas := doc.Call("getElementById", "canvas")
+	// bodyW := doc.Get("body").Get("clientWidth").Float()
+	// bodyH := doc.Get("body").Get("clientHeight").Float()
+	bodyW := js.Global().Get("innerWidth").Float() * 0.9
+	bodyH := js.Global().Get("innerHeight").Float() * 0.9
+	log.Println(bodyW, bodyH)
+	canvas.Set("width", bodyW)
+	canvas.Set("height", bodyH)
+	ctx := canvas.Call("getContext", "2d")
+
+	files := []string{
+		"/data/gopher_body_normal.png",
+		"/data/gopher_doteye.png",
+		"/data/gopher_xeye.png",
+	}
+	images = make(map[string]js.Value)
+	for _, file := range files {
+		key := filepath.Base(file)
+		key = strings.TrimSuffix(key, ".png")
+		images[key] = js.Global().Call("eval", "new Image()")
+		images[key].Set("src", "data:image/png;base64,"+loadImage(file))
+	}
+
+	// canvas.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	// 	js.Global().Get("window").Call("alert", "Don't click me!")
+	// 	return nil
+	// }))
+
+	n := 0
+	js.Global().Call("setInterval", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		drawGopher(ctx, 0, 0)
+		n = 1 - n
+		return nil
+	}), js.ValueOf(50))
+
+	select {}
+}
+
+func drawGopher(cctx js.Value, x, y int) {
+	cctx.Call("clearRect", x, y, 200, 200)
+	cctx.Call("drawImage", images["gopher_body_normal"], x, y)
+	if r.Intn(2) != 0 {
+		cctx.Call("drawImage", images["gopher_doteye"], x-10, y)
+	} else {
+		cctx.Call("drawImage", images["gopher_doteye"], x+10, y)
+	}
+
+}
 
 func loadImage(path string) string {
 	href := js.Global().Get("location").Get("href")
@@ -37,50 +100,4 @@ func loadImage(path string) string {
 		log.Fatal(err)
 	}
 	return base64.StdEncoding.EncodeToString(b)
-}
-
-func main() {
-	document := js.Global().Get("document")
-	canvas := document.Call("getElementById", "canvas")
-	ctx := canvas.Call("getContext", "2d")
-	canvas.Set("width", js.ValueOf(500))
-	canvas.Set("height", js.ValueOf(500))
-
-	images := make([]js.Value, 4)
-	files := []string{
-		"/data/out01.png",
-		"/data/out02.png",
-		"/data/out03.png",
-		"/data/out02.png",
-	}
-	for i, file := range files {
-		images[i] = js.Global().Call("eval", "new Image()")
-		images[i].Set("src", "data:image/png;base64,"+loadImage(file))
-	}
-
-	canvas.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		js.Global().Get("window").Call("alert", "Don't click me!")
-		return nil
-	}))
-
-	n := 0
-	js.Global().Call("setInterval", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		ctx.Call("clearRect", 0, 0, 500, 500)
-		ctx.Call("drawImage", images[n%4], 0, 0)
-		n++
-
-		style := canvas.Get("style")
-		left := style.Get("left")
-		if left.Equal(js.Undefined()) {
-			left = js.ValueOf("0px")
-		} else {
-			n, _ := strconv.Atoi(strings.TrimRight(left.String(), "px"))
-			left = js.ValueOf(fmt.Sprintf("%dpx", n+10))
-		}
-		style.Set("left", left)
-
-		return nil
-	}), js.ValueOf(50))
-
-	select {}
 }
